4cef18fc62455e56722407ae16cbeca0f7617184
陈慧 <495818445@qq.com>
2025-07-11 13:45:45 +0800

    first commit

---
+++ src/app/(web)/page.tsx
@@ -0,0 +1,13 @@
+import { Hero } from "@/components/hero";
+import { ResourceList } from "@/components/resource";
+
+export const revalidate = 60;
+
+export default function Home() {
+	return (
+		<>
+			<Hero />
+			<ResourceList />
+		</>
+	);
+}
